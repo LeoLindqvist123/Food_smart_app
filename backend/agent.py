84a2f2f34f7ca1bfd7734a4e4c62a8638d16ea2c
Leo Lindqvist Kröhnert <leolikro01@gmail.com>
2026-05-10 23:06:11 +0200

    first class

--- backend/agent.py
+++ backend/agent.py
@@ -1,12 +1,20 @@
 from pydantic_ai import Agent
 from dotenv import load_dotenv
+from backend.constants import MODEL
 
 load_dotenv()
-MODEL = "openrouter:nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free"
+
+class FoodResult(BaseModel):
+    name_food: str
+    calories: int
+    ingredient: list
+    recipe:str
+    protein: int
+    fat: int
 
 food_agent = Agent(
     model = MODEL,
     system_prompt=(
         "You are a intelligent and smart scanner"
-    ),
+    )output_type=FoodResult,
 )
